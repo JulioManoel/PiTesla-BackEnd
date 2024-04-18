b2b61af0cc0f6c182a8f7e9987f402172fdb08ed
JulioManoel <juliobonow@gmail.com>
2024-04-18 18:32:58 -0300

    Remove

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,3 @@
 <?php
 use App\Http\Controllers\SchoolController;
 use Illuminate\Support\Facades\Route;
-
-Route::controller(SchoolController::class)->group(function () {
-  Route::get('/school', 'index');
-  Route::post('/school', 'store');
-});

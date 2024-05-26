c580c3cee68d1cea0c5cca91770febd10109a227
JulioManoel <juliobonow@gmail.com>
2024-05-26 18:24:41 -0300

    landing page

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing-page');
+});
+
+Route::get('/privacy-policy', function () {
+    return view('privacy-policy');
+});
+
+Route::get('/terms-conditions', function () {
+    return view('terms-conditions');
 });
